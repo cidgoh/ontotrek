2350401cc57f581a279cb5ce13dcf756b88ef29d
Damion Dooley <damion.dooley@bccdc.ca>
2018-09-05 18:21:13 -0700

    initial setup

---
+++ js/index.js
@@ -0,0 +1,18 @@
+THREE = require('three');
+SpriteText = require('three-spritetext');
+ForceGraph3D = require('3d-force-graph')
+/*
+const Graph = ForceGraph3D()
+  (document.getElementById('3d-graph'))
+    .jsonUrl('../datasets/miserables.json')
+    .nodeAutoColorBy('group')
+    .nodeThreeObject(node => {
+      const sprite = new SpriteText(node.id);
+      sprite.color = node.color;
+      sprite.textHeight = 8;
+      return sprite;
+    });
+
+// Spread nodes a little wider
+Graph.d3Force('charge').strength(-150);
+*/
