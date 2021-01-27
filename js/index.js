5a359fba5a4daec4ac38e12361cdc11813489949
Damion Dooley <damion.dooley@bccdc.ca>
2021-01-27 03:48:18 -0800

    revamp of code for latest 3d-force-graph

--- js/index.js
+++ js/index.js
@@ -1,18 +1,3 @@
 THREE =        require('../../3d-force-graph/node_modules/three');
 SpriteText =   require('../../3d-force-graph/node_modules/three-spritetext');
 //ForceGraph3D = require('../../3d-force-graph/src/3d-force-graph.js')
-/*
-const Graph = ForceGraph3D()
-  (document.getElementById('3d-graph'))
-    .jsonUrl('../datasets/miserables.json')
-    .nodeAutoColorBy('group')
-    .nodeThreeObject(node => {
-      const sprite = new SpriteText(node.id);
-      sprite.color = node.color;
-      sprite.textHeight = 8;
-      return sprite;
-    });
-
-// Spread nodes a little wider
-Graph.d3Force('charge').strength(-150);
-*/
